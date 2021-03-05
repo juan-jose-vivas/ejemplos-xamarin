8fb2e80fda69b968115a960d58771d9da2063484
juan-jose-vivas <67050527+juan-jose-vivas@users.noreply.github.com>
2021-03-05 12:00:07 +0100

    Create Cancion.cs

---
+++ Cancion.cs
@@ -0,0 +1,10 @@
+using System;
+namespace xamarin_bindings4.Models
+{
+    public class Cancion
+    {
+        public String Imagen { get; set; }
+        public String Artista { get; set; }
+        public String Titulo { get; set; }
+    }
+}
